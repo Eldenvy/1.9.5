f950cd3140ddde919ae44f4d2183f74f3ad160b2
BespiatchykEugeniiEugenievich <fullmetalancient@gmail.com>
2022-06-25 15:46:49 +1000

    Initial commit

---
+++ app/src/main/java/com/example/a195/MyDraw.java
@@ -0,0 +1,65 @@
+package com.example.a195;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+    public class MyDraw extends View {
+
+        int N = 50; // количество шариков
+        float[] x  = new float[N];
+        float[] y  = new float[N];
+        float[] vx = new float[N];
+        float[] vy = new float[N];
+        public MyDraw(Context context) {
+            super(context);
+            fillRandom(x, 0, 500);
+            fillRandom(y, 0, 500);
+            fillRandom(vx, -3, 3);
+            fillRandom(vy, -3, 3);
+        }
+        // Возвращает псевдослучайное число
+        // в полуинтервале [min, max)
+        float rand(float min , float max){
+            return (float)(Math.random() * (max - min + 1)) + min;
+        }
+        // Заполняет массив псевдослучайными числами в полуинтервале [min, max)
+        void fillRandom(float[] array , float min, float max){
+            for (int i = 0; i < array.length; i++){
+                array[i] = rand (min, max);
+            }
+        }
+        // Прибавляет  к элементам первого массива значения элеметов второго
+        // Массивы должны быть одинаковой длины!
+        void add(float[] array , float[] values){
+            for (int i = 0; i < array.length; i++){
+                array[i] += values[i];
+            }
+        }
+        void drawBalls(Canvas canvas)
+        {
+            for (int i = 0; i < N; i++) {
+                canvas.drawCircle(x[i], y[i], 20, paint);
+            }
+        }
+
+        Paint paint = new Paint();
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            // отрисовываем все шарики
+            drawBalls(canvas);
+            // готовим массивы x и у для следущего кадра
+            add(x, vx);
+            add(y, vy);
+            //запрашиваем перерисовку
+            invalidate();
+        }
+    }
+
